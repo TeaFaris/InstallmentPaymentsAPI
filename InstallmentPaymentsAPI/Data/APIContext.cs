d2320d70f745645e3524bede0d46dd4637547781
TeaFaris <95927550+TeaFaris@users.noreply.github.com>
2023-03-24 04:02:35 +0500

    DBSet added to database context

--- InstallmentPaymentsAPI/Data/APIContext.cs
+++ InstallmentPaymentsAPI/Data/APIContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InstallmentPaymentsAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace InstallmentPaymentsAPI.Data
 {
 	public class APIContext : DbContext
 	{
+		public DbSet<Category> Categories { get; set; }
+
 		public APIContext(DbContextOptions<APIContext> Options)
 			: base(Options)
 		{
