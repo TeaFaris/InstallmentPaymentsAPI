0c61ade1fbe5c12caf08a467578af36da3d07c6c
TeaFaris <95927550+TeaFaris@users.noreply.github.com>
2023-03-24 14:39:24 +0500

    Migrated database

--- InstallmentPaymentsAPI/Migrations/20230324093846_PaymentChange.Designer.cs
+++ InstallmentPaymentsAPI/Migrations/20230324093846_PaymentChange.Designer.cs
@@ -2,6 +2,7 @@
 using InstallmentPaymentsAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InstallmentPaymentsAPI.Migrations
 {
     [DbContext(typeof(APIContext))]
-    partial class APIContextModelSnapshot : ModelSnapshot
+    [Migration("20230324093846_PaymentChange")]
+    partial class PaymentChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
